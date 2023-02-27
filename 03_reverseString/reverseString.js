b20d367dfd86d0231686d9b67631de1e0ffaac87
g145773 <andrew.j.laguana@kp.org>
2023-02-27 14:13:08 -0800

    Exercise 3 finished, Ex 4 baseline created

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,13 @@
 const reverseString = function(input) {
+let stringReversed = ""
 
+for (let i = input.length; i != 0; i--) {
+  stringReversed += input.substr(i-1,1);  //length is one place off for getting letter position
+}
+return stringReversed;
 };
 
-console.log("Hello");
+document.getElementById("demo").innerHTML = reverseString("Andy");
+
 // Do not edit below this line
 //module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,5 +2,6 @@ const removeFromArray = function() {
 
 };
 
+document.getElementById("demo").innerHTML = removeFromArray("Andy");
 // Do not edit below this line
 module.exports = removeFromArray;
