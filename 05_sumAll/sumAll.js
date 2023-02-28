1c26502e2931ee2695c649d8202450216ef6b33b
g145773 <andrew.j.laguana@kp.org>
2023-02-28 07:43:54 -0800

    Exercise 5 finished, Ex 6 baseline created

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,25 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+let sum = 0;
+let lower = numberOne;
+let upper = numberTwo;
+if (Number.isInteger(numberOne) &&
+    Number.isInteger(numberTwo) &&
+    numberOne >= 0 &&
+    numberTwo >= 0) {
+    if (numberTwo < numberOne) {
+        lower = numberTwo;
+        upper = numberOne;
+        }
 
+    for (i = lower; i < upper + 1; i++)
+        {
+        console.log(i + " " + lower + " " + upper);
+        sum += i;}
+
+    return sum;
+} else  {return "ERROR"}
 };
 
-document.getElementById("demo").innerHTML = sumAll();
+//document.getElementById("demo").innerHTML = sumAll(4,[9]);
 // Do not edit below this line
-//module.exports = sumAll;
+module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function() {
 
 };
-
+document.getElementById("demo").innerHTML = sumAll(4,[9]);
 // Do not edit below this line
-module.exports = leapYears;
+//module.exports = leapYears;
