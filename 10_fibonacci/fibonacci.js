614bc142a95194fce62ceb491a7944e8dda0c0f3
g145773 <andrew.j.laguana@kp.org>
2023-02-28 15:49:14 -0800

    Exercise 10 finished, Ex 11 baseline created

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+//1,1,2,3,5,8,13
+let numberOne = 1;
+let numberTwo = 1;
+if (number<0) return "OOPS"
+for (let i =1; i < number-1 ; i++) {
+let temp = numberTwo;
+numberTwo = numberOne + numberTwo;
+numberOne = temp;
 
+     }
+
+return numberTwo;
 };
-document.getElementById("demo").innerHTML = palindromes("racecar");
+//document.getElementById("demo").innerHTML = fibonacci("6");
 // Do not edit below this line
-//module.exports = fibonacci;
+module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,6 @@
 const getTheTitles = function() {
 
 };
-
+document.getElementById("demo").innerHTML = getTheTitles("6");
 // Do not edit below this line
 module.exports = getTheTitles;
