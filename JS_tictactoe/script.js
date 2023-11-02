3da3df08b36da239c94d274a9c66b1dff878a70b
g145773 <andrew.j.laguana@kp.org>
2023-11-02 15:59:14 -0700

    Initial for Tic Tac Toe

---
+++ JS_tictactoe/script.js
@@ -0,0 +1,10 @@
+// const gameBoard;
+//
+// displayController;
+
+const playerFactory = (name, symbol) => {
+  return { name, symbol };
+};
+
+const playerOne = playerFactory("andy", "x");
+const playerTwo = playerFactory("hal", "o");
